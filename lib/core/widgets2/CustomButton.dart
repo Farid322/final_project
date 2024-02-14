73ff07b700e3d5e393cf26b0be73edabb3dd4a71
yasminemagdy2 <110430249+yasminemagdy2@users.noreply.github.com>
2024-02-15 00:32:14 +0200

    yarab

---
+++ lib/core/widgets2/CustomButton.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/constant/constant.dart';
+
+class Custombutton extends StatelessWidget {
+  const Custombutton({
+    super.key,
+    required this.text,
+    this.onTap,
+    required this.width,
+    required this.hieght,
+    required this.fontsize,
+    required this.radius,
+    required this.color,
+    this.shadow,
+    required this.icon,
+    this.schedulewidth,
+    required this.herozintal,
+    required this.textcolor,
+  });
+  final double herozintal;
+  final String text;
+  final double width;
+  final double hieght;
+  final double fontsize;
+  final double radius;
+  final Color? color;
+  final IconData? icon;
+  final List<BoxShadow>? shadow;
+  final void Function()? onTap;
+  final double? schedulewidth;
+  final Color textcolor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: hieght,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+          child: Row(
+            children: [
+              SizedBox(
+                width: schedulewidth,
+                height: 20,
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: herozintal, vertical: 5),
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: textcolor,
+                    fontFamily: 'Roboto',
+                    fontSize: fontsize,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+// const Color(0xFF0E5C6D)
+//shadow
+// color: Color(0x210E5C6D),
+           // blurRadius: 13,
+            //offset: Offset(0, 1),
+            //spreadRadius: 0,
--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graduation_project/Features/patienthome/screens/views/accpted_result_page.dart';
+import 'package:graduation_project/Features/patienthome/screens/views/My_session.dart';
+import 'package:graduation_project/Features/patienthome/screens/views/session.dart';
 
 void main() {
-  runApp( const Hemodialysis());
+  runApp(const Hemodialysis());
 }
+
 class Hemodialysis extends StatelessWidget {
   const Hemodialysis({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return      const ScreenUtilInit(
+    return const ScreenUtilInit(
       designSize: Size(375, 812),
       minTextAdapt: true,
-      child:  MaterialApp(
-        home: AccptedResultPage(),
+      child: MaterialApp(
+        home: session(),
         debugShowCheckedModeBanner: false,
       ),
     );

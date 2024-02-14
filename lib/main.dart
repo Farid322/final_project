627d835bfea2f2b9581857839e1982c3782b908f
Farid322 <152017031+Farid322@users.noreply.github.com>
2024-02-14 21:48:55 +0200

    tttt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:graduation_project/Features/patienthome/screens/views/accpted_result_page.dart';
+import 'package:graduation_project/core/routes/app_routes.dart';
 
 void main() {
   runApp( const Hemodialysis());
@@ -11,11 +11,11 @@ class Hemodialysis extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return      const ScreenUtilInit(
-      designSize: Size(375, 812),
+    return      ScreenUtilInit(
+      designSize: const Size(375, 812),
       minTextAdapt: true,
-      child:  MaterialApp(
-        home: AccptedResultPage(),
+      child:  MaterialApp.router(
+        routerConfig: AppRouter.router,
         debugShowCheckedModeBanner: false,
       ),
     );
